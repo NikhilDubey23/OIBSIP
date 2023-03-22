0cfe7118d1de50b2112fd66795db3dd2961d5c77
NikhilDubey23 <126020287+NikhilDubey23@users.noreply.github.com>
2023-03-22 15:27:32 +0530

    js code of calcultor

---
+++ script.js
@@ -0,0 +1,17 @@
+let result = document.getElementById("result");
+
+function getResult(num) {
+    result.value += num;
+}
+
+function clearResult() {
+    result.value = "";
+}
+
+function calculate() {
+    try {
+        result.value = eval(result.value);
+    } catch (error) {
+        result.value = "Error";
+    }
+}
